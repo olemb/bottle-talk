b10b86eaab3a325c1349d96929b087b7d8c717f1
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-10-10 15:11:41 +0200

    Added server.py.

---
+++ server.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import os
+import bottle
+from bottle import get, response, static_file, SimpleTemplate
+
+def get_file_paths():
+    files = []
+    for dirpath, dirnames, filenames in os.walk('.'):
+        if '.git' in dirnames:
+            dirnames.remove('.git')
+
+        for filename in filenames:
+            if filename.endswith('.pyc'):
+                continue
+
+            if filename.endswith('~'):
+                continue
+
+            path = os.path.join(dirpath, filename)
+            if path.startswith('./'):
+                path = path[2:]
+
+            files.append(path)
+
+    return files
+
+@get('/slides.html')
+def slides():
+    print('Dynamic!')
+    return os.popen('rst2html'
+                    ' --stylesheet=slider/slides.css'
+                    ' --link-stylesheet'
+                    ' --template=template.txt'
+                    ' slides.rst')
+
+@get('/<path:path>')
+def files(path):
+    return static_file(path, '.')
+
+file_list_template = SimpleTemplate("""
+
+<h1>Bottle!</h1>
+
+<p><a href="slides.html">Start presentation</a></p>
+
+<ul>
+  % for file in files:
+    <li><a href="{{file}}">{{file}}</a></li>
+  % end
+</ul>
+""")
+
+@get('/')
+def file_list():
+    return file_list_template.render(files=get_file_paths())
+
+if __name__ == '__main__':
+    bottle.debug(True)
+    bottle.run(reloader=True)
