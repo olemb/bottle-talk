40ac4a4a3c0dc074a9bd3f22062620f2ba212729
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-10-10 17:37:09 +0200

    Reorganized server.py a bit.

--- server.py
+++ server.py
@@ -4,6 +4,37 @@ import fnmatch
 import bottle
 from bottle import get, response, static_file, SimpleTemplate
 
+file_list_template = SimpleTemplate("""
+<!doctype html>
+<html>
+<head><title>Bottle</title></head>
+<link rel="stylesheet" href="uit/uit.css" type="text/css" />
+<link rel="stylesheet" href="slider/slides.css" type="text/css" />
+<body>
+
+<div id="uit_triangle"><img src="uit/triangle.png"></div>
+<div id="uit_logo"><img src="uit/logo.png"></div>
+
+<div class="section">
+<h1>Bottle!</h1>
+
+<p><a href="slides.html" style="font-size: 400%">Start presentation</a></p>
+
+<p><a href="server.py">server.py</a></p>
+
+<h2>Files</h2>
+
+<ul>
+  % for file in files:
+    <li><a href="{{file}}">{{file}}</a></li>
+  % end
+</ul>
+</div>
+
+</body>
+</html>
+""")
+
 def ignore_file(filename):
     # Can't use .gitignore here because it ignores slides.html
     # and not .git and .gitignore.
@@ -44,37 +75,6 @@ def slides():
 @get('/<path:path>')
 def files(path):
     return static_file(path, '.')
-
-file_list_template = SimpleTemplate("""
-<!doctype html>
-<html>
-<head><title>Bottle</title></head>
-<link rel="stylesheet" href="uit/uit.css" type="text/css" />
-<link rel="stylesheet" href="slider/slides.css" type="text/css" />
-<body>
-
-<div id="uit_triangle"><img src="uit/triangle.png"></div>
-<div id="uit_logo"><img src="uit/logo.png"></div>
-
-<div class="section">
-<h1>Bottle!</h1>
-
-<p><a href="slides.html" style="font-size: 400%">Start presentation</a></p>
-
-<p><a href="server.py">server.py</a></p>
-
-<h2>Files</h2>
-
-<ul>
-  % for file in files:
-    <li><a href="{{file}}">{{file}}</a></li>
-  % end
-</ul>
-</div>
-
-</body>
-</html>
-""")
 
 @get('/')
 def file_list():
